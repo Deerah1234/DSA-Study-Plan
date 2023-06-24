2865df94840f303cd75b02db0fc1b36945a5809a
Emmanuel James <chiderajames55@gmail.com>
2023-06-24 06:08:18 +0100

    add: initializations of arrays in c

--- src/Week-1-2_Data-Structures/Arrays/Basics/Intro/initilization.c
+++ src/Week-1-2_Data-Structures/Arrays/Basics/Intro/initilization.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main() {
-	// initialization
+	//NOTE - initialization
+	// int initialization of array
 	int a[50] = { 1, 2, 3, 4, 5 };
+	// char initialization of array
+	char b[50] = { 'a', 'b', 'c', 'd', 'e' };
+	// float initialization of array
+	float c[50] = { 1.1, 2.1, 3.2, 4.3, 5.5 };
 }
