58f4c959d06320191519aa0beee8c4f8f6f4baaf
Emmanuel James <chiderajames55@gmail.com>
2023-06-24 05:56:13 +0100

    add: basic introductions practices of arrays

---
+++ src/Week-1-2_Data-Structures/Arrays/Basics/Intro/decalaration.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	//NOTE - declarations of array variables 
+	// int array declaration
+	int a[50];
+	// char array declaration
+	char b[50];
+	// float array declaration
+	float c[50];
+}
---
+++ src/Week-1-2_Data-Structures/Arrays/Basics/Intro/initilization.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+	// initialization
+	int a[50] = { 1, 2, 3, 4, 5 };
+}
