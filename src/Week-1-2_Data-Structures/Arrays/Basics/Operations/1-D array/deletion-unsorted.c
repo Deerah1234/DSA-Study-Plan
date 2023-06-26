71b151e19065951f9af6e44f9a47f837e5d4f04a
Emmanuel James <chiderajames55@gmail.com>
2023-06-26 16:49:18 +0100

    add: bound checking for unsorted

--- src/Week-1-2_Data-Structures/Arrays/Basics/Operations/1-D array/deletion-unsorted.c
+++ src/Week-1-2_Data-Structures/Arrays/Basics/Operations/1-D array/deletion-unsorted.c
@@ -3,24 +3,67 @@
 
 int main(void)
 {
-	int array[50] = { 6, 2, 0, 4, 5 };
-	//NOTE - inline declaration
-	int size = 5, pos = 2;
-
-	for (int i = 0; i <= size-1; i++)
-	{
-		printf("%d\t", array[i]);
-	}
 	printf("\n");
-	
-	array[pos-1] = array[size-1];
-	size--;
-
-	for (int i = 0; i <= size-1; i++)
-	{
-		printf("%d\t", array[i]);
-	}
+    printf(" +---------------------------------------------------------+\n");
+    printf(" |                                                         |\n");
+    printf(" |               deletion of unsorted Array                |\n");
+    printf(" |                                                         |\n");
+    printf(" +---------------------------------------------------------+\n");
 	printf("\n");
+
+	int array[50], size_the_array, pos;
+
+	// size of the array
+	printf("Enter size of array: ");
+	scanf("%d", &size_the_array);
+
+	// initialize the array
+	if (size_the_array > 50 || size_the_array == 0) 
+	{
+		printf("invalid size\n");
+	} 
+	else 
+	{
+		for (int i = 0; i < size_the_array; i++)
+		{
+			printf("index[%d]: ", i + 1);
+			scanf("%d", &array[i]);
+		}
+
+		// print the array before the deletion
+		printf("Array before deletion: \n");
+		printf("===================== \n");
+		for (int i = 0; i <= size_the_array-1; i++)
+		{
+			printf("%d\t", array[i]);
+		}
+		printf("\n");
+
+		// position of the arrary to delete
+		printf("Enter position to delete: ");
+		scanf("%d", &pos);
+
+		// check position
+		if (pos >= size_the_array || pos <= 0) 
+		{
+			printf("invalid position\n");
+		}
+		else 
+		{
+			// delete the element at specified position of unsorted array
+			array[pos-1] = array[size_the_array-1];
+			size_the_array--;
+
+			// print the array after deletion
+			printf("Array after deletion: \n");
+			printf("===================== \n");
+			for (int i = 0; i <= size_the_array-1; i++)
+			{
+				printf("%d\t", array[i]);
+			}
+			printf("\n");
+		}
+	}
 	
 	return (0);
 }
