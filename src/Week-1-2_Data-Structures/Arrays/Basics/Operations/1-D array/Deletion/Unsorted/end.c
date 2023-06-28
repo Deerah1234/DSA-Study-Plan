5246e69226dc00fee8e81bc59457108f0e679448
Emmanuel James <chiderajames55@gmail.com>
2023-06-28 05:26:38 +0100

    add: unsorted operation at end

---
+++ src/Week-1-2_Data-Structures/Arrays/Basics/Operations/1-D array/Deletion/Unsorted/end.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	int array[50], size_of_array;
+
+	// size of array
+	printf("Enter size of the array: ");
+	scanf("%d", &size_of_array);
+
+	// check if size of array is greater than 50 bytes or less than 1 byte
+	if (size_of_array > 50 || size_of_array == 0)
+	{
+		printf("Invaild size\n");
+	}
+	else
+	{
+		// Accept the array elements
+		printf("Enter elements of array: ");
+		printf("\n==========================\n");
+		for (int idx = 0; idx < size_of_array; idx++)
+		{
+			printf("index[%d]: ", idx + 1);
+			scanf("%d", &array[idx]);
+		}
+
+		// print the array before deletion
+		printf("Array before deletion");
+		printf("\n==========================\n");
+		for (int idx = 0; idx < size_of_array; idx++)
+		{
+			printf("%d\t", array[idx]);
+		}
+		printf("\n");
+
+		// Delete elements of array at the end
+		size_of_array--;
+
+		// Print the array after deletion
+		printf("Array after deletion");
+		printf("\n==========================\n");
+		for (int idx = 0; idx < size_of_array; idx++)
+		{
+			printf("%d\t", array[idx]);
+		}
+		printf("\n");
+
+	}
+	
+	return (0);
+}
