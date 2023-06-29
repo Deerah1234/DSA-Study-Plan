623a65c57ad68c4a32ff8129f4cf7098408e2970
Emmanuel James <chiderajames55@gmail.com>
2023-06-29 11:21:40 +0100

    add: intro to string

---
+++ src/Week-1-2_Data-Structures/Strings/Intro/decalaration.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	char string[10];
+	char *string;
+	return (0);
+}
---
+++ src/Week-1-2_Data-Structures/Strings/Intro/initilization.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	char string[10] = "Jenny";
+	char string2[10] = {'J', 'e', 'n', 'n', 'y', '\0'};
+	char string3[ ] = "Jenny";
+	char *string4 = "Jenney";
+	return (0);
+}
