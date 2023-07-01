7c3190254e56188a25701b7fc52715bb9c27a28a
Emmanuel James <chiderajames55@gmail.com>
2023-07-01 15:41:20 +0100

    add: comment

--- src/Week-1-2_Data-Structures/Arrays/Basics/Operations/2-D array/traversal.c
+++ src/Week-1-2_Data-Structures/Arrays/Basics/Operations/2-D array/traversal.c
@@ -6,6 +6,7 @@ int main(void)
 	int array[2][3] = { {1, 2, 3}, {4, 5, 6} };
 	int num_of_rows = 2, num_of_cols = 3;
 
+	// accessing the array
 	for (int row = 0; row < num_of_rows; row++)
 	{
 		for (int col = 0; col < num_of_cols; col++)
