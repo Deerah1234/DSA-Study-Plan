cc0d6dfb7acb17c8c7a7edacc5837b6012dd7087
Emmanuel James <chiderajames55@gmail.com>
2023-07-18 05:18:00 +0100

    update: modify the head pointer

--- src/Week-1-2_Data-Structures/Linked Lists/Basics/Intro/Dobule/create.c
+++ src/Week-1-2_Data-Structures/Linked Lists/Basics/Intro/Dobule/create.c
@@ -3,59 +3,59 @@
 
 struct node
 {
-	int data;
-	struct node *next;
-	struct node *prev;
+    int data;
+    struct node* next;
+    struct node* prev;
 };
 
-void create(struct node *head);
-void display(struct node *head);
-
+void create(struct node** head);
+void display(struct node* head);
 
 int main(void)
-{	
-	struct node *head = 0;
-	create(head);
-	display(head);
-	return (0);
+{
+    struct node* head = NULL;
+    create(&head);
+    display(head);
+    return 0;
 }
 
-void create(struct node *head)
+void create(struct node** head)
 {
-	struct node *newnode, *temp;
-	int choice = 1;
+    struct node* newnode;
+    struct node* temp;
+    int choice = 1;
 
-	while (choice)
-	{
-		newnode = (struct node *)malloc(sizeof(struct node));
-		printf("Enter date: ");
-		scanf("%d", &newnode->data);
-		newnode -> prev = 0;
-		newnode -> next = 0;
+    while (choice)
+    {
+        newnode = (struct node*)malloc(sizeof(struct node));
+        printf("Enter data: ");
+        scanf("%d", &newnode->data);
+        newnode->prev = NULL;
+        newnode->next = NULL;
 
-		if (head == 0)
-		{
-			head = temp = newnode;
-		}
-		else
-		{
-			temp -> next = newnode;
-			newnode -> prev = temp;
-			temp = newnode;
-		}
+        if (*head == NULL)
+        {
+            *head = temp = newnode;
+        }
+        else
+        {
+            temp->next = newnode;
+            newnode->prev = temp;
+            temp = newnode;
+        }
 
-		printf("Do you want to contine?: (0 or 1): ");
-		scanf("%d", &choice);
-	}
+        printf("Do you want to continue? (0 or 1): ");
+        scanf("%d", &choice);
+    }
 }
 
-void display(struct node *head) 
+void display(struct node* head)
 {
-	struct node *temp = head;
+    struct node* temp = head;
 
-	while ( temp != 0 )
-	{
-		printf("%d\t", temp->data);
-		temp = temp->next;
-	}
+    while (temp != NULL)
+    {
+        printf("%d\t\n", temp->data);
+        temp = temp->next;
+    }
 }
