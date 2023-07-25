79f9a4ff8ba88cf22655c309c6d92f75f5b4cff9
Emmanuel James <chiderajames55@gmail.com>
2023-07-25 04:48:46 +0100

    Update: add all implementation to the main file

--- src/Week-1-2_Data-Structures/Linked Lists/Basics/Operations/Dobuly/Insert/main.c
+++ src/Week-1-2_Data-Structures/Linked Lists/Basics/Operations/Dobuly/Insert/main.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "util.h"
 
-// struct node
-// {
-// 	int data;
-// 	struct node *next;
-// 	struct node *prev;
-// } *head, *tail;
+struct node
+{
+	int data;
+	struct node *next;
+	struct node *prev;
+};
 
-void display();
-// void insertAtBeg();
-// void insertAtEnd();
-// void insertAtPos();
+
+void display(struct node *head);
+struct node * insertAtBeg(struct node *head);
+struct node * insertAtEnd(struct node *tail);
+void insertAtPos(struct node *head);
+void lengthNode(struct node * head);
+
 
 int main(void)
 {
 	struct node *head, *tail, *newnode;
 	head = 0;
-	int choice = 1;
+	int choice = 1; 
 
 	while (choice)
 	{
@@ -47,13 +49,15 @@ int main(void)
 		printf("\nOriginal Lists:\n");
 		display(head);
 		printf("\nLists after insection:\n");
-		head = insertAtBeg(head);
+		insertAtPos(head);
+		lengthNode(head);
 		display(head);
 	}
 	
 	return (0);
 }
 
+// Display the element in the list
 void display(struct node *head)
 {
     struct node *temp = head;
@@ -66,51 +70,74 @@ void display(struct node *head)
     printf("\n");
 }
 
-// void insertAtBeg()
-// {
-// 	struct node *newnode;
-// 	newnode = malloc(sizeof(struct node));
-// 	printf("Enter data you want to insect at the beginnig: ");
-// 	scanf("%d", &newnode->data);
-// 	newnode->next = 0, newnode->prev = 0;
+// Insert Element at the beginning of the list
+struct node * insertAtBeg(struct node *head)
+{
+	struct node *newnode;
+	newnode = malloc(sizeof(struct node));
+	printf("Enter data you want to insect at the beginnig: ");
+	scanf("%d", &newnode->data);
+	newnode->next = 0, newnode->prev = 0;
 
-// 	head->prev = newnode;
-// 	newnode->next = head;
-// 	head = newnode;
-// }
+	head->prev = newnode;
+	newnode->next = head;
+	head = newnode;
 
-// void insertAtEnd()
-// {
-// 	struct node *newnode;
-// 	newnode = malloc(sizeof(struct node));
-// 	printf("Enter data you want to insect at the end: ");
-// 	scanf("%d", &newnode->data);
-// 	newnode->next = 0, newnode->prev = 0;
+	return head;
+}
 
-// 	tail->next = newnode;
-// 	newnode->prev = tail;
-// 	tail = newnode;
-// }
+// Insert element at the end of the list
+struct node * insertAtEnd(struct node *tail)
+{
+	struct node *newnode;
+	newnode = malloc(sizeof(struct node));
+	printf("Enter data you want to insect at the end: ");
+	scanf("%d", &newnode->data);
+	newnode->next = 0, newnode->prev = 0;
 
-// void insertAtPos()
-// {
-// 	struct node *newnode, *temp = head;
-// 	int pos, i = 1;
-// 	printf("Enter postion: ");
-// 	scanf("%d", &pos);
+	tail->next = newnode;
+	newnode->prev = tail;
+	tail = newnode;
 
-// 	newnode = malloc(sizeof(struct node));
-// 	printf("Enter data you want to insect at the position: ");
-// 	scanf("%d", &newnode->data);
-// 	newnode->next = 0, newnode->prev = 0;
+	return tail;
+}
 
-// 	while (i < pos-1)
-// 	{
-// 		temp = temp->next;
-// 		i++;
-// 	}
-// 	newnode->prev = temp;
-// 	newnode->next = temp->next;
-// 	temp->next = newnode;
-// 	newnode->next->prev = newnode;
-// }
+// Insert element at a given position in the list
+void insertAtPos(struct node *head)
+{
+	struct node *newnode, *temp = head;
+	int pos, idx = 1;
+	printf("Enter postion: ");
+	scanf("%d", &pos);
+
+	newnode = malloc(sizeof(struct node));
+	printf("Enter data you want to insect at the position: ");
+	scanf("%d", &newnode->data);
+	newnode->next = 0, newnode->prev = 0;
+
+	while (idx < pos-1)
+	{
+		temp = temp->next;
+		idx++;
+	}
+
+	newnode->prev = temp;
+	newnode->next = temp->next;
+	temp->next = newnode;
+	newnode->next->prev = newnode;
+}
+
+// Get the length of the list
+void lengthNode(struct node * head)
+{
+	int count = 0;
+	struct node *temp = head;
+
+	while (temp != 0)
+	{
+		temp = temp->next;
+		count++;
+	}
+
+	printf("The length of the Dobuly LL is: %d\n", count);
+}
