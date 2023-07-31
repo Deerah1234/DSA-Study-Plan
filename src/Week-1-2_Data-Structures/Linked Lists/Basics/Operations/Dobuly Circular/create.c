9dc6d621ed7bb5da619cd18f093b67e2cd01ca70
Emmanuel James <chiderajames55@gmail.com>
2023-07-31 05:27:33 +0100

    add: create function to create node

---
+++ src/Week-1-2_Data-Structures/Linked Lists/Basics/Operations/Dobuly Circular/create.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct node
+{
+	int data;
+	struct node *next;
+	struct node *prev;
+} *head, *tail;
+
+void display();
+void createDCLL();
+
+
+int main(void)
+{
+	createDCLL();
+	display();
+	return (0);
+}
+
+void createDCLL()
+{
+	struct node *newnode;
+	head = 0;
+	int choice = 1;
+
+	while (choice)
+	{
+		newnode = malloc(sizeof(struct node));
+		printf("Enter date: ");
+		scanf("%d", &newnode->data);
+
+		if ( head == 0 )
+		{
+			head = tail = newnode;
+			head->next = head;
+			head->prev = head;
+		}
+		else
+		{
+			tail->next = newnode;
+			newnode->prev = tail;
+			newnode->next = head;
+			head->prev = newnode;
+			tail = newnode;
+		}
+		
+		printf("Do you want to continue? (0 or 1): ");
+        scanf("%d", &choice);
+	}
+}
+
+void display()
+{
+	struct node *temp;
+	temp = head;
+
+	while (temp != tail)
+	{
+		printf("%d\t", temp->data);
+		temp = temp->next;
+	}
+	printf("%d\n", temp->data);
+}
