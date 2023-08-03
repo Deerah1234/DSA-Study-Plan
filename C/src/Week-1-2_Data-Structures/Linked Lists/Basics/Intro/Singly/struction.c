2b7c3b5701195be3b240b0da4795df43dc2bec7a
Emmanuel James <chiderajames55@gmail.com>
2023-08-03 04:52:34 +0100

    add: commets

--- C/src/Week-1-2_Data-Structures/Linked Lists/Basics/Intro/Singly/struction.c
+++ C/src/Week-1-2_Data-Structures/Linked Lists/Basics/Intro/Singly/struction.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-
+// The structure type of a singly linked list type
+/*
+	data: contains the date of the node.
+	next: contains the pointer to the next node.
+*/
 struct node
 {
 	int data;
