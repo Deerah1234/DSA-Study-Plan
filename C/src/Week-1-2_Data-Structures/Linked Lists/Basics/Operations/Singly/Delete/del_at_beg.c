0c95ff953d03f99eb250c1bee4c123d3db290be6
Emmanuel James <chiderajames55@gmail.com>
2023-08-11 04:52:49 +0100

    add: delete operation for singly LL

---
+++ C/src/Week-1-2_Data-Structures/Linked Lists/Basics/Operations/Singly/Delete/del_at_beg.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node 
+{
+	int data;
+	struct node *next;
+} *head;
+
+void delAtBeg();
+void display();
+
+int main()
+{
+	head = 0;
+	struct node *temp, *newnode;
+	int choice = 1;
+
+	while (choice)
+	{
+		newnode = malloc(sizeof(struct node));
+		printf("Enter date: ");
+		scanf("%d", &newnode->data);
+		newnode->next = 0;
+
+		if (head == 0)
+		{
+			head = temp = newnode;	
+		}
+		else
+		{
+			temp->next = newnode;
+			temp = newnode;
+		}
+		
+		printf("Do you want to continue? (0 or 1): ");
+		scanf("%d", &choice);
+	}
+
+	if (!choice)
+	{
+		display();
+		delAtBeg();
+		display();
+	}
+}
+
+void display()
+{
+	struct node *temp = head;
+
+	while (temp != 0)
+	{
+		printf("%d\t", temp->data);
+		temp = temp->next;
+	}
+	printf("\n");
+}
+
+
+void delAtBeg()
+{
+	struct node *temp = head;
+
+	head = head->next;
+	free(temp);
+}
