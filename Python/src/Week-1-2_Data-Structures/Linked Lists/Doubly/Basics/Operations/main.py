6e31636e01f30b624fdc68f0ef734709a9919011
Emmanuel James <chiderajames55@gmail.com>
2023-08-25 06:52:53 +0100

    add: Dobuly LL operations

--- Python/src/Week-1-2_Data-Structures/Linked Lists/Doubly/Basics/Operations/main.py
+++ Python/src/Week-1-2_Data-Structures/Linked Lists/Doubly/Basics/Operations/main.py
@@ -28,9 +28,51 @@ class DoublyLinkedList:
             self.tail.next = new_node
             self.tail = new_node
         self.length += 1
+        return
+
+    def pop(self) -> Node:
+        if self.length == 0:
+            return None
+        temp = self.tail
+        if self.length == 1:
+            self.head = self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+            temp.prev = None
+        self.length -= 1
+        return temp
+
+    def prepend(self, value: int) -> bool:
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        self.length += 1
         return True
+
+    def pop_first(self) -> Node:
+        if self.length == 0:
+            return None
+        temp = self.head
+        if self.length == 1:
+            self.head = self.tail = None
+        else:        
+            self.head = self.head.next
+            self.head.prev = None
+            temp.next = None
+        self.length -= 1
+        return temp
 
 
 my_doubly_linked_list = DoublyLinkedList(7)
-my_doubly_linked_list.append(6)
+# my_doubly_linked_list.append(6)
+# my_doubly_linked_list.append(8)
+# my_doubly_linked_list.append(9)
+my_doubly_linked_list.print_list()
+my_doubly_linked_list.pop_first()
+my_doubly_linked_list.pop_first()
 my_doubly_linked_list.print_list()
