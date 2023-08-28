0554c0ca803a07c9ac2469fe4b0ff3173aa30f78
Emmanuel James <chiderajames55@gmail.com>
2023-08-28 04:45:25 +0100

    add: more operations

--- Python/src/Week-1-2_Data-Structures/Linked Lists/Doubly/Basics/Operations/main.py
+++ Python/src/Week-1-2_Data-Structures/Linked Lists/Doubly/Basics/Operations/main.py
@@ -60,19 +60,74 @@ class DoublyLinkedList:
         temp = self.head
         if self.length == 1:
             self.head = self.tail = None
-        else:        
+        else:
             self.head = self.head.next
             self.head.prev = None
             temp.next = None
         self.length -= 1
         return temp
 
+    def get(self, index: int) -> Node:
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        if index < self.length / 2:
+            for _ in range(index):
+                temp = temp.next
+        else:
+            temp = self.tail
+            for _ in range(self.length - 1, index, -1):
+                temp = temp.prev
+        return temp
+
+    def set_value(self, index: int, value: int) -> bool:
+        if temp := self.get(index):
+            temp.value = value
+            return True
+        return False
+
+    def insert(self, index: int, value: int) -> bool:
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+
+        new_node = Node(value)
+        before = self.get(index - 1)
+        after = before.next
+
+        new_node.prev = before
+        new_node.next = after
+        before.next = new_node
+        after.prev = new_node
+
+        self.length += 1
+        return True
+
+    def remove(self, index: int) -> Node:
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+
+        temp = self.get(index)
+
+        temp.prev.next = temp.next
+        temp.next.prev = temp.prev
+        temp.prev = temp.next = None
+
+        self.length -= 1
+        return temp.value
+
 
 my_doubly_linked_list = DoublyLinkedList(7)
-# my_doubly_linked_list.append(6)
-# my_doubly_linked_list.append(8)
-# my_doubly_linked_list.append(9)
+my_doubly_linked_list.append(6)
+my_doubly_linked_list.append(8)
+my_doubly_linked_list.append(9)
 my_doubly_linked_list.print_list()
-my_doubly_linked_list.pop_first()
-my_doubly_linked_list.pop_first()
+my_doubly_linked_list.remove(1)
 my_doubly_linked_list.print_list()
