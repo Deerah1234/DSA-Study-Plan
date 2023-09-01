f36448ec19f01cba743c65d1b696a2c1c6343b6b
Emmanuel James <chiderajames55@gmail.com>
2023-09-01 04:26:55 +0100

    fix

--- Python/src/Week-1-2_Data-Structures/Linked Lists/Singly/Basics/Operations/main.py
+++ Python/src/Week-1-2_Data-Structures/Linked Lists/Singly/Basics/Operations/main.py
@@ -117,7 +117,7 @@ class LinkedList:
         temp = self.head
         self.head = self.tail
         self.tail = temp
-        
+
         after = temp
         before = None
         for _ in range(self.length):
@@ -126,17 +126,29 @@ class LinkedList:
             before = temp
             temp = after
 
+    def find_kth_from_end(self, k: int) -> Node:
+        fast = slow = self.head
+
+        for _ in range(k):
+            if fast is None:
+                return None
+            fast = fast.next
+
+        while fast:
+            slow = slow.next
+            fast = fast.next
+
+        return slow.value
+
 
 my_linked_list = LinkedList(1)
 
-# my_linked_list.append(2)
-# my_linked_list.append(3)
-# my_linked_list.append(3)
-# my_linked_list.append(4)
-# my_linked_list.append(4)
-# my_linked_list.append(5)
-# my_linked_list.prepend(0)
-# my_linked_list.print_list()
-# my_linked_list.remove_deplicate()
-my_linked_list.pop()
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(3)
+my_linked_list.append(44)
+my_linked_list.append(4)
+my_linked_list.append(5)
+my_linked_list.prepend(0)
 my_linked_list.print_list()
+print(my_linked_list.find_kth_from_end(3), end="\n")
