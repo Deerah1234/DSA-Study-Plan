779bf0340a5186da1aea7a2d0c23490e28186ec5
Emmanuel James <chiderajames55@gmail.com>
2023-09-21 04:19:46 +0100

    update: more operations

--- C/src/Week-1-2_Data-Structures/Deque/main.c
+++ C/src/Week-1-2_Data-Structures/Deque/main.c
@@ -8,6 +8,9 @@ int f = -1, r = -1;
 void enqueueFront(int);
 void enqueueReal(int);
 void display();
+void getFront();
+void dequeueFront();
+void dequeueReal();
 
 int main()
 {
@@ -18,7 +21,11 @@ int main()
     enqueueReal(-1);
     enqueueReal(0);
     enqueueFront(7);
+    display();
 
+    dequeueFront();
+    dequeueReal();
+    dequeueFront();
     display();
     return (0);
 }
@@ -71,13 +78,69 @@ void enqueueReal(int x)
     }
 }
 
+// Use (r + 1) % N to correctly handle the wrap-around condition
+// void display()
+// {
+//     int i = f;
+//     do
+//     {
+//         printf("%d\t", deque[i]);
+//         i = (i + 1) % N;
+//     } while (i != (r + 1) % N);
+//     printf("\n");
+// }
+// TODO: CHECK WHY IT'S STILL DISPLAYING DELETED VALUES.
 void display()
 {
     int i = f;
-    do
+    while (i != r)
     {
         printf("%d\t", deque[i]);
-        i = (i + 1) % N; 
-    } while (i != (r + 1) % N); // Use (r + 1) % N to correctly handle the wrap-around condition
-    printf("\n");
+        i = (i + 1) % N;
+    }
+    printf("%d\n", deque[i]);
+}
+
+void dequeueFront()
+{
+    if (f == -1 && r == -1)
+    {
+        printf("Queue is empty");
+    }
+    else if (f == r)
+    {
+        f = r = -1;
+    }
+    else if (f == N - 1)
+    {
+        printf("%d\n", deque[f]);
+        f = 0;
+    }
+    else
+    {
+        printf("%d\n", deque[f]);
+        f++;
+    }
+}
+
+void dequeueReal()
+{
+    if (f == -1 && r == -1)
+    {
+        printf("Queue is empty");
+    }
+    else if (f == r)
+    {
+        f = r = -1;
+    }
+    else if (r == 0)
+    {
+        printf("%d\n", deque[r]);
+        r = N - 1;
+    }
+    else
+    {
+        printf("%d\n", deque[r]);
+        r++;
+    }
 }
